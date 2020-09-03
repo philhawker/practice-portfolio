fb60d912bb31843f4b004cde38928d6c783f4911
philhawker <philhawker1211@gmail.com>
2020-09-03 16:43:10 -0600

    created home route using switch and Route

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
 import PortfolioContainer from './portfolio/portfolio-container'
 import NavigationContainer from './navigation/navigation-container'
+import Home from './pages/home'
+import AboutMe from './pages/about-me'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavigationContainer />
+
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={AboutMe} />
+            </Switch>
+
+          </div>
+        </Router>
+
         <h1>Hawker Family Blog</h1>
         <h2>Christ | Covenants | Faith</h2>
         <PortfolioContainer />
