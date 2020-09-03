f17347dcbc148f2048b81ea50359afe2d2b42426
philhawker <philhawker1211@gmail.com>
2020-09-03 13:28:33 -0600

    changed heading and subheading

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Hawker Family Blog</h1>
+        <h2>Christ | Covenants | Faith</h2>
       </div>
     );
   }
