4e5e11f1882bb64747f65c442aa99221408e295c
philhawker <philhawker1211@gmail.com>
2020-09-03 14:36:09 -0600

    created portfolio item component

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react"
 
+import PortfolioItem from "./portfolio-item"
+
 export default class PortfolioContainer extends Component {
   render() {
     return (
       <div>
-        <h2>Portfolio items go here...</h2>
+        <h2>Portfolio items go here update...</h2>
+
+        <PortfolioItem />
+        <PortfolioItem />
+        <PortfolioItem />
       </div>
     )
   }
---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,11 @@
+// This is an example of a functional component. meaning this is just for creating a function in this file
+import React from "react"
+
+export default function () {
+  // render statement is not required in a simple functional component
+  return (
+    <div>
+      <h3>Portfolio item</h3>
+    </div>
+  )
+}
