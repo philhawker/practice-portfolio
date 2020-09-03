3ecf292c710638c2d63a40a19892b6a517ab50ad
philhawker <philhawker1211@gmail.com>
2020-09-03 16:01:53 -0600

    created state for categories and built filter handler

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -9,24 +9,43 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "MISSION STATEMENT",
       data: [
-        { title: "Education" },
-        { title: "Mental Health" },
-        { title: "Childrearing" },
-        { title: "Church" }
+        { title: "For Such a Time As This", category: "Education" },
+        { title: "Learning The Healer's Art", category: "Mental Health" },
+        { title: "Praise To The Man", category: "Childrearing" },
+        { title: "Enemies, Part 7: Fear Is A Formidable Foe", category: "Church" }
       ]
     }
+
+    this.handleFilter = this.handleFilter.bind(this)
   }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(topic => {
+        return topic.category === filter
+      })
+    })
+  }
+
   blogTopics() {
     return this.state.data.map(topic => {
       return <BlogTopic title={topic.title} url={"google.com"} />
     })
   }
 
+
   render() {
     return (
       <div>
         <h2>{this.state.pageTitle}</h2>
+
+        <button onClick={() => this.handleFilter('Education')} >Education</button>
+        <button onClick={() => this.handleFilter('Mental Health')}>Mental Health</button>
+        <button onClick={() => this.handleFilter('Childrearing')}>Childrearing</button>
+        <button onClick={() => this.handleFilter('Church')}>Church</button>
         {this.blogTopics()}
+
+
       </div>
     )
   }
