2cc0fbf0a384cc4e1b3f84e4a7edd738539be242
philhawker <philhawker1211@gmail.com>
2020-09-03 22:22:06 -0600

    linked to api server

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom"
+import axios from 'axios'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 
 import NavigationContainer from './navigation/navigation-container'
@@ -17,7 +14,24 @@ import PortfolioDetail from './portfolio/portfolio-detail'
 import NoMatch from './pages/no-match'
 
 export default class App extends Component {
+  constructor() {
+    super()
+
+    this.getPortfolioItems = this.getPortfolioItems.bind(this)
+  }
+
+  getPortfolioItems() {
+    axios.get("https://philhawker.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        console.log("response data", response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   render() {
+    this.getPortfolioItems()
     return (
       <div className='app'>
 
